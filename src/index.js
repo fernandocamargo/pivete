5afd7c75ef7382b993db4ff543a873f2cc4e5922
Fernando Camargo Del Buono <fernandocamargodelbuono@Fernandos-MacBook-Pro.local>
2022-04-20 16:34:37 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { Root } from "components";
+import { measure } from "tools";
+
+export const initialize = () =>
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <Root />
+    </StrictMode>
+  );
+
+export default document.fonts.ready
+  .then(initialize)
+  .then(measure.bind(this, process.env.DEBUGGING && console.log));
