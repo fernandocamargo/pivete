f1042ca5742e72670a5a47b418e23f3c155c5c1b
Fernando Camargo <camargodelbuono@gmail.com>
2022-04-21 09:28:03 +0200

    Initial shape of table

--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,1 +1,35 @@
-export default () => "Root";
+import random from "lodash/random";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Table from "react-pivottable/PivotTableUI";
+import "react-pivottable/pivottable.css";
+
+export const serialize = (response) => response.json();
+
+export default () => {
+  const [data, setData] = useState();
+  const [settings, setSettings] = useState();
+  const derivedAttributes = useMemo(
+    () => ({
+      "Budget vs. Forecast": () => random(1, 1000),
+    }),
+    []
+  );
+  const onChange = useCallback((event) => setSettings(event), []);
+
+  useEffect(() => {
+    const persist = (response) => setData(response.data);
+
+    window.fetch("/assets/json/mock/data.json").then(serialize).then(persist);
+  }, []);
+
+  return data ? (
+    <Table
+      data={data}
+      derivedAttributes={derivedAttributes}
+      onChange={onChange}
+      {...settings}
+    />
+  ) : (
+    "Loading..."
+  );
+};
